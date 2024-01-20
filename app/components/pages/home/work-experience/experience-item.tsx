8b1abcfb9d2c8e40298c3d0ce6996454c95a8608
Renato Torres <renatotorres1@outlook.com>
2024-01-20 10:28:06 -0300

    experiencias profissionais

---
+++ app/components/pages/home/work-experience/experience-item.tsx
@@ -0,0 +1,69 @@
+import { TechBadge } from "@/app/components/tech-badge"
+import Image from "next/image"
+
+type ExpInfoProps = {
+  img_src: string,
+    link: string,
+    text_link: string,
+    area: string,
+    tempo: string,
+    desc: string,
+    tech: {
+      tech1: string,
+      tech2: string,
+      tech3: string,
+      tech4: string,
+      tech5: string,
+    },
+}
+
+export const ExperienceItem = ({ img_src, link, text_link, area, tempo, desc, tech }: ExpInfoProps) => {
+  return (
+    <div className="grid grid-cols-[40px,1fr] gap-4 md:gap-10">
+      <div className="flex flex-col items-center gap-4">
+        <div className="rounded-full border border-gray-500 p-0.5">
+          <Image
+            src={img_src}
+            width={40}
+            height={40}
+            className="rounded-full"
+            alt="Logo Cubo9"
+          />
+        </div>
+
+        <div className="h-full w-[1px] bg-gray-800"/>
+      </div>
+
+      <div>
+        <div className="flex flex-col text-sm sm:text-base">
+          <a
+            href={link}
+            target="_blank"
+            className="text-gray-500 hover:text-emerald-500 transition-colors"
+          >
+            {text_link}
+          </a>
+          <h4 className="text-gray-300 mb-3 mt-6">
+            {area}
+          </h4>
+          <span className="text-gray-500 mb-3">
+            {tempo}
+          </span>
+          <p className="text-gray-400">
+            {desc}
+          </p>
+
+          <p className="text-gray-400 text-sm mb-3 mt-6 font-semibold">Competências</p>
+          
+          <div className="flex gap-x-2 gap-y-3 flex-wrap lg:max-w-[350px] mb-8">
+            <TechBadge name={tech.tech1}/>
+            <TechBadge name={tech.tech2}/>
+            <TechBadge name={tech.tech3}/>
+            <TechBadge name={tech.tech4}/>
+            <TechBadge name={tech.tech5}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ app/components/pages/home/work-experience/index.tsx
@@ -0,0 +1,62 @@
+import { SectionTitle } from "@/app/components/section-title"
+import { ExperienceItem } from "./experience-item"
+
+const ExpInfo = [
+  {
+    img_src: "/images/cubo9.png",
+    link: "https://www.linkedin.com/company/cubo9/",
+    text_link: "@Cubo9®",
+    area: "Front-End Developer",
+    tempo: "ago 2023 • O momento • (6 meses)",
+    desc: "Desenvolvendo e realizando manutenções nos projetos utilizando ReactJS, NextJS, SCSS e TypeScript. Sempre organizando o projeto com sprints organizadas pelo Trello.",
+    tech: {
+      tech1: "React.js",
+      tech2: "Next.js",
+      tech3: "SCSS",
+      tech4: "TypeScript",
+      tech5: "Git",
+    },
+  },
+  {
+    img_src: "/images/renatoTorres.png",
+    link: "https://www.linkedin.com/in/renato-torres-dev/",
+    text_link: "@renato-torres-dev",
+    area: "Front-End Developer",
+    tempo: "out 2021 • O momento • (2 anos e 3 meses)",
+    desc: "Perfil pessoal onde desenvolvo projetos Freelance e pessoais, demonstrando técnicas para melhorar a performance UI/UX para o cliente.",
+    tech: {
+      tech1: "HTML5",
+      tech2: "Next.js",
+      tech3: "CSS3",
+      tech4: "TailwindCSS",
+      tech5: "JavaScript",
+    },
+  },
+]
+
+export const WorkExperience = () => {
+  return (
+    <section className="container py-16 flex gap-16">
+      <div className="max-w-[420px]">
+        <SectionTitle subtitle="jobs" title="Experiência Profissional" />
+        <p className="text-gray-400 mt-6">
+          Sempre em busca de novos conhecimentos, novas experiências e novos projetos. Gosto de solucionar e entregar o projeto, mais do que perfeito, para satisfazer a todos que admiram o meu trabalho.
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        {ExpInfo.map(( info, index) => (
+          <ExperienceItem 
+            img_src={info.img_src}
+            link={info.link}
+            text_link={info.text_link}
+            area={info.area}
+            tempo={info.tempo}
+            desc={info.desc}
+            tech={info.tech}
+          />
+        ))}
+      </div>
+    </section>    
+  )
+}
